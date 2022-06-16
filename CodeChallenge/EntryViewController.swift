82305c37e4bdd02e23ee7eea680ac67a8bafba06
Allan Ray Jasa <allan.jasa@akqa.com>
2022-06-16 12:10:55 +0200

    Camera taking photos and using it

--- CodeChallenge/EntryViewController.swift
+++ CodeChallenge/EntryViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class EntryViewController: UIViewController {
+class EntryViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var currentDate: UILabel!
@@ -15,15 +15,30 @@ class EntryViewController: UIViewController {
     @IBOutlet weak var currencyTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
     @IBAction func takePhoto(_ sender: Any) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func save(_ sender: Any) {
+        
+    }
+    
+    //MARK: - Image Picker Controller Delegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true)
+        imageView.image = info[.originalImage] as? UIImage
     }
 }
 
