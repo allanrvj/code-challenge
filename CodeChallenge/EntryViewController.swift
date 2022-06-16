7f782df026140a1bf38c1fd7b8dfe9af903987f2
Allan Ray Jasa <allan.jasa@akqa.com>
2022-06-16 11:53:59 +0200

    Input page initial layout

---
+++ CodeChallenge/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Allan Jasa on 16/06/2022.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var currentDate: UILabel!
+    @IBOutlet weak var totalTextField: UITextField!
+    @IBOutlet weak var currencyTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func takePhoto(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+}
+
